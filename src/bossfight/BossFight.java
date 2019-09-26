bcb38d2511481af7e430b4c641318b098423b703
priyanut buadum <priyanutbuadum@MacBook-Pro-khxng-priyanut.local>
2019-09-26 14:18:29 +0700

    initialize the project

---
+++ src/bossfight/BossFight.java
@@ -0,0 +1,12 @@
+
+package bossfight;
+
+
+public class BossFight {
+
+    public static void main(String[] args) {
+        Customize ctm = new Customize();
+        ctm.armour();
+    }
+    
+}
---
+++ src/bossfight/Customize.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bossfight;
+import java.util.Scanner;
+/**
+ *
+ * @author int101
+ */
+public class Customize {
+    
+    double atk;
+    double def;
+    double eva;
+    int startingPoints;
+    int healthPotions;
+    String[] HeadArmourArray;
+    double[] HeadArmourDefValue;
+    double[] HeadArmourEvasion;
+    String[] ChestArmourArray;
+    double[] ChestArmourDefValue;
+    double[] ChestArmourEvasion;
+    Scanner scn;
+    int armourIndex;
+    
+    Customize() {
+        this.scn = new Scanner(System.in);
+        //armour
+            //head
+            HeadArmourArray = new String[]{"Iron Helmet","Feather Berret"};
+            HeadArmourDefValue = new double[]{5,2};
+            HeadArmourEvasion = new double[] {-2,4};
+            //chest
+            ChestArmourArray = new String[]{"Plate Armour","Leather Armour","No Amour"};
+            ChestArmourDefValue = new double[]{12,8,0};
+            ChestArmourEvasion = new double[] {-5,8,14};
+            
+    }
+    
+    public void armour() {
+        //Helmet
+        System.out.println("[Choose Your Helmet]");
+       // System.out.println(HeadArmourArray[0]+"[1]"+" "+HeadArmourArray[1]+"[2]");
+         int max;
+         max = HeadArmourArray.length;
+        StringBuilder headStr = new StringBuilder();
+         for (int i=0; i < max; i++) {
+            int n = i;
+            headStr.append(HeadArmourArray[0]);
+            headStr.append("[").append(n+1).append("] ");
+    }
+         System.out.println(headStr);
+        
+        
+        
+        armourIndex = scn.nextInt();
+      /*  boolean minCheck = false;
+        boolean maxCheck = false;
+        if (armourIndex < 1) minCheck = true;
+        if (armourIndex > 2) maxCheck = true;
+        if (minCheck || maxCheck){
+            System.out.println("INVALID");
+            return; 
+        }
+else */
+
+            if (!isValid(1,max,armourIndex))return;
+            armourIndex -= 1;
+             this.def += HeadArmourDefValue[armourIndex];
+             this.eva += HeadArmourEvasion[armourIndex];
+            System.out.println("you have choosen "+HeadArmourArray[armourIndex]);
+            showTotal();
+        
+        //Chest Armour
+        
+    }
+    
+    private boolean isValid(int min,int max,int checkNum) {
+        boolean minCheck = false;
+        boolean maxCheck = false;
+        if (armourIndex < min) minCheck = true;
+        if (armourIndex > max) maxCheck = true;
+        if (minCheck || maxCheck){
+            System.out.println("INVALID");
+            return false; 
+        }
+        else
+        return true;
+    }
+    
+    public void showTotal() {
+        StringBuilder total = new StringBuilder();
+        total.append("[Current Stat]");
+        total.append("\nDef : ");
+        total.append(this.def);
+        total.append("\tEva : ");
+        total.append(this.eva);
+        System.out.println(total);
+    }
+        
+    
+    
+    
+    
+}
